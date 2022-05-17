5ba732fe4af17c66491aeb32c7371659aa88b229
Wolf Van Herreweghe <wolfvh@getupgamesofficial.com>
2022-05-17 16:35:09 +0200

    Update keymap

--- keyboards/wolf/turtle_pad/turtle_pad.h
+++ keyboards/wolf/turtle_pad/turtle_pad.h
@@ -27,9 +27,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     K30, K31, K32, K33,      \
     K40, K41,      K43, K44  \
 ) { \
-    { K00,   K01,   K02,   K03,   K04   }, \
-    { K10,   K11,   K12,   K13,   KC_NO }, \
-    { K20,   K21,   K22,   K23,   K24   }, \
-    { K30,   K31,   K32,   K33,   KC_NO }, \
-    { K40,   K41,   KC_NO, K43,   K44   }  \
+    { K00, K01, K02,   K03, K04   }, \
+    { K10, K11, K12,   K13, KC_NO }, \
+    { K20, K21, K22,   K23, K24   }, \
+    { K30, K31, K32,   K33, KC_NO }, \
+    { K40, K41, KC_NO, K43, K44   }  \
 }
