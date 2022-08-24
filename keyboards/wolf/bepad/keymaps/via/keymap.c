7832bc7f0a7ecd048d33cef404829d5a55ca013f
wolf <wolfvanh@gmail.com>
2022-08-24 12:41:50 +0200

    Push Base BePad

--- keyboards/wolf/bepad/keymaps/via/keymap.c
+++ keyboards/wolf/bepad/keymaps/via/keymap.c
@@ -2,22 +2,22 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    [0] = LAYOUT_60_all(
+    [0] = LAYOUT_5(
         KC_LEFT, KC_UP, KC_RGHT, KC_ENT,
         KC_DOWN
     ),
 
-    [1] = LAYOUT_60_all(
+    [1] = LAYOUT_5(
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS
     ),
 
-    [2] = LAYOUT_60_all(
+    [2] = LAYOUT_5(
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS
     ),
 
-    [3] = LAYOUT_60_all(
+    [3] = LAYOUT_5(
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS
     )
---
+++ keyboards/wolf/bepad/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xA7, 0xB5, 0xD0, 0x40, 0x78, 0x32, 0xFD, 0xD3}
+
+/* CONSIDER ADDING AN UNLOCK COMBO. SEE DOCUMENTATION. */
+#define VIAL_INSECURE
